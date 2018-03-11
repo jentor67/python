f735a458976a5b9121dc9ecb29575c3271577ec0
John Major <john.jentor@gmail.com>
2018-03-11 15:18:47 -0500

    checkDigit program for ISBN 10

--- checkDigit.py
+++ checkDigit.py
@@ -15,6 +15,33 @@ def selectVersion(question):
 
     return selection
 
+def ISBN10():
+    print("Calculating ISBN10 check digit:")
+    Number = "A"
+
+    while( Number.isdigit() == False ):
+        Number = selectVersion("Input ISBN 10 Number: ")
+        if( Number.isdigit() == True and len(Number) == 9 ):
+            i = 0
+            sumProduct = 0
+            while( i < 9 ):
+                theProduct = 10-i
+                sumProduct = sumProduct + int(Number[i])*theProduct
+                print("The digit "+str(Number[i])+" * "+str(theProduct))
+                i = i +1
+            print("The sum Product is "+str(sumProduct)) 
+            checkDigit = 11 - sumProduct % 11
+            print("The Check Digit is "+str(checkDigit))
+            break
+        else:
+            print(" ")
+            if( Number.isdigit() == True ): 
+                print("***WARNING*** Input does not contain 9 digits, please try again.")
+            else:
+                print("***WARNING*** Input is not all digits, please try again.")
+            Number = "A"
+            
+    
 
 def UPCBarcode():
     print("Calculating UPC barcode check digit:")
@@ -53,19 +80,21 @@ def UPCBarcode():
             print(" ")
             break
         else:
-            Number = "A"
             print(" ")
             if( Number.isdigit() == True ): 
                 print("***WARNING*** Input does not contain 11 digits, please try again.")
             else:
                 print("***WARNING*** Input is not all digits, please try again.")
+            Number = "A"
 
 
 def doSelection(n):
     if ( n == 0 ) :
         print("Good by")
-    elif ( n ==1 ):
+    elif ( n == 1 ):
         UPCBarcode()
+    elif ( n == 2 ):
+        ISBN10()
     else:
         print(" ")
         print("***Warning*** Choice '"+str(n)+"' does not exit, please try again.")
@@ -77,6 +106,7 @@ choice=9999
 while choice > 0:
     print("0. End")
     print("1. UPC Barcode")
+    print("2. ISBN 10 Barcode")
     print(" ")
     choice = selectVersion(question1)
 
